d61169cdaf9f63cf6b89a3da6aa32f1cdfebc6cd
clerdomy <clerdomz@gmail.com>
2024-05-31 12:13:58 -0400

    Modelando o Sistema Bancário em POO com Python

--- banco.py
+++ banco.py
@@ -1,105 +1,144 @@
-
 import time
 from datetime import datetime
 from prettytable import PrettyTable 
 
+class ContaBancaria:
+    LIMITE_SAQUES = 3
 
-saldo = 0
-limite = 500
-numero_saques = 0
-LIMITE_SAQUES = 3
-extrato = PrettyTable(["Data", "Historico", "Valor (R$)"])
+    def __init__(self, saldo=0, limite=500):
+        self.saldo = saldo
+        self.limite = limite
+        self.numero_saques = 0
+        self.extrato = PrettyTable(["Data", "Historico", "Valor (R$)"])
 
-menu = """
+    def print_extrato(self):
+        print("\n================ EXTRATO ================")
+        print(f"DISPONIVEL\n\t=Total Disponivel\t {self.saldo}\nÚltimos lançamentos Conta Fácil(C/C + Poupança)")
+        print(self.extrato)
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    def get_error(self, valor):
+        prefixo = 'Operação falhou!'
+        erro = {'has_error': False, 'mensagem_erro': ''}
+        
+        dict_erros = {
+            1: '' if self.saldo >= valor else 'Saldo insuficiente.',
+            2: '' if self.numero_saques < self.LIMITE_SAQUES else 'Número máximo de saques atingido.',
+            3: '' if valor > 0 else 'O valor do saque deve ser superior a zero.',
+            4: '' if self.limite >= valor else 'O valor do saque excede o limite permitido.'
+        }
+        
+        mensagens_erro = [msg for msg in dict_erros.values() if msg]
+        
+        if mensagens_erro:
+            if len(mensagens_erro) > 1:
+                erro['mensagem_erro'] = f'{prefixo}\n' + '\n'.join(f' - {mensagem}' for mensagem in mensagens_erro)
+            else:
+                erro['mensagem_erro'] = f'{prefixo} {mensagens_erro[0]}'
+            
+            erro['has_error'] = True
 
-=> """
+        return erro
 
+    def saque(self, valor):
+        error = self.get_error(valor)
+        if error['has_error']:
+            print(error['mensagem_erro'])
+            return
+        
+        self.saldo -= valor
+        print("Saque realizado com sucesso")
+        self.extrato.add_row([datetime.now().strftime("%x"), "Sacar", f"-{valor}"]) 
+        self.numero_saques += 1
 
-def print_extrato():
-    global extrato, saldo
-    print("\n================ EXTRATO ================")
-    print(f"""DISPONIVEL\n\t=Total Disponivel\t {saldo}
-    \nÚltmos lançamentos Conta Fácil(C/C + Poupança)""")
-    print(extrato)
+    def depositar(self, valor):
+        if valor <= 0:
+            print("Valor deve ser maior que R$0")
+            return
+            
+        self.saldo += valor
+        self.extrato.add_row([datetime.now().strftime("%x"), "Depositar", f"{valor}"]) 
+        print(f'R${valor} Depositado com sucesso')
 
-def get_error(saldo, valor, limite, numero_saques):
-    global LIMITE_SAQUES
+class Usuario:
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+        self.conta = ContaBancaria()
 
-    prefixo = 'Operação falhou!'
-    erro = {'has_error': False, 'mensagem_erro': ''}
+    def __str__(self):
+        return f'Usuário: {self.nome}, CPF: {self.cpf}'
+
+def cadastrar_usuario(usuarios):
+    nome = input("Informe o nome do usuário: ")
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = Usuario(nome, cpf)
+    usuarios.append(usuario)
+    print(f'Usuário {nome} cadastrado com sucesso!')
+
+def selecionar_usuario(usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    for usuario in usuarios:
+        if usuario.cpf == cpf:
+            return usuario
+    print("Usuário não encontrado.")
+    return None
+
+def main():
+    usuarios = []
+    menu_principal = """
     
-    # Dicionário de erros
-    dict_erros = {
-        1: '' if saldo >= valor else 'Saldo insuficiente.',
-        2: '' if numero_saques < LIMITE_SAQUES else 'Número máximo de saques atingido.',
-        3: '' if valor > 0 else 'O valor do saque deve ser superior a zero.',
-        4: '' if limite >= valor else 'O valor do saque excede o limite permitido.'
-    }
+    [c] Cadastrar Usuário
+    [l] Logar em Conta
+    [q] Sair
     
-    # Filtra as mensagens de erro
-    mensagens_erro = [msg for msg in dict_erros.values() if msg]
+    => """
+
+    menu_conta = """
     
-    if mensagens_erro:
-        if len(mensagens_erro) > 1:
-            erro['mensagem_erro'] = f'{prefixo}\n' + '\n'.join(f' - {mensagem}' for mensagem in mensagens_erro)
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [v] Voltar
+    
+    => """
+
+    while True:
+        opcao = input(menu_principal)
+
+        if opcao == "c":
+            cadastrar_usuario(usuarios)
+
+        elif opcao == "l":
+            usuario = selecionar_usuario(usuarios)
+            if usuario:
+                while True:
+                    opcao_conta = input(menu_conta)
+
+                    if opcao_conta == "d":
+                        valor = int(input('Informe o valor de deposito: '))
+                        time.sleep(.3)
+                        usuario.conta.depositar(valor)
+
+                    elif opcao_conta == "s":
+                        valor = int(input('Informe o valor do saque: '))
+                        time.sleep(.3)
+                        usuario.conta.saque(valor)
+
+                    elif opcao_conta == "e":
+                        usuario.conta.print_extrato()
+                        time.sleep(.3)
+
+                    elif opcao_conta == "v":
+                        break
+
+                    else:
+                        print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+        elif opcao == "q":
+            break
+
         else:
-            erro['mensagem_erro'] = f'{prefixo} {mensagens_erro[0]}'
-        
-        erro['has_error'] = True
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
 
-    return erro
-
-def saque(valor):
-    global extrato, saldo, limite, numero_saques
-    error = get_error(saldo,  valor, limite, numero_saques)
-    if error['has_error']:
-        print(error['mensagem_erro'])
-        return
-    
-    saldo-= valor
-    print("Saque realizado com sucesso")
-    extrato.add_row([datetime.now().strftime("%x"), "Sacar", f"-{valor}"]) 
-    numero_saques +=1
-
-def depositar(valor):
-    global saldo, extrato
-
-    if valor <= 0:
-        print("Valor deve ser maior que R$0")
-        return
-        
-    saldo += valor
-    extrato.add_row([datetime.now().strftime("%x"), "Depositar", f"{valor}"]) 
-    print(f'R${valor} Depositado com sucesso')
-
-
-while True:
-
-    opcao = input(menu)
-
-    if opcao == "d":
-        valor = int(input('Informe o valor de deposito: '))
-        time.sleep(.3)
-        depositar(valor)
-
-
-
-    elif opcao == "s":
-        valor = int(input('Informe o valor do saque: '))
-        time.sleep(.3)
-        saque(valor)
-
-    elif opcao == "e":
-        print_extrato()
-        time.sleep(.3)
-
-    elif opcao == "q":
-        break
-
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+if __name__ == "__main__":
+    main()
